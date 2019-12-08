c5249ad90e70f14232c5aaa187730014228a54eb
Ryan O'Beirne <ryanobeirne@ryanobeirne.com>
2019-12-08 00:47:57 -0500

    day02: part1

--- src/day02.rs
+++ src/day02.rs
@@ -1,7 +1,110 @@
 use advent_of_code_2019::*;
+use std::fs::read_to_string;
 
 fn main() -> Result<()> {
+    let input = read_to_string("input/day02.txt")?;
+    let mut program = Program::from(input);
+    
+    while let Some(()) = program.step() {
 
-    answer!(1, 1, 42);
+    }
+
+    let first = program.data.get(0).unwrap();
+
+    answer!(1, 1, first);
+    Ok(())
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Program{
+    data: Vec<usize>,
+    head: usize,
+}
+
+impl Program {
+    fn step(&mut self) -> Option<()> {
+        let output = Command {
+            opcode: OpCode::from(*self.data.get(self.head)?),
+            input_a: *self.data.get(*self.data.get(self.head + 1)?)?,
+            input_b: *self.data.get(*self.data.get(self.head + 2)?)?,
+        }.output()?;
+
+        let output_index = *self.data.get(self.head + 3)?;
+        *self.data.get_mut(output_index)? = output;
+
+        self.head += 4;
+
+        Some(())
+    }
+}
+
+impl From<String> for Program {
+    fn from(s: String) -> Self {
+        let data = s.split(',')
+            .filter_map(|n| n.parse::<usize>().ok())
+            .collect();
+
+        Program {
+            data,
+            head: 0,
+        }
+    }
+}
+
+struct Command {
+    opcode: OpCode,
+    input_a: usize,
+    input_b: usize,
+}
+
+impl Command {
+    fn output(&self) -> Option<usize> {
+        match self.opcode {
+            OpCode::Add => Some(self.input_a + self.input_b),
+            OpCode::Mul => Some(self.input_a * self.input_b),
+            OpCode::Halt => None,
+        }
+    }
+}
+
+impl From<[usize; 3]> for Command {
+    fn from(arr: [usize; 3]) -> Self {
+        Command {
+            opcode: OpCode::from(arr[0]),
+            input_a: arr[1],
+            input_b: arr[2],
+        }
+    }
+}
+
+enum OpCode {
+    Add,
+    Mul,
+    Halt,
+}
+
+impl From<usize> for OpCode {
+    fn from(u: usize) -> Self {
+        match u {
+            1 => OpCode::Add,
+            2 => OpCode::Mul,
+            99 => OpCode::Halt,
+            x => unreachable!("Something went wrong!: {}", x),
+        }
+    }
+}
+
+#[test]
+fn first_steps() -> Result<()> {
+    let mut program = Program::from("1,9,10,3,2,3,11,0,99,30,40,50".to_string());
+    let expected = Program::from("1,9,10,70,2,3,11,0,99,30,40,50".to_string());
+
+    program.step().unwrap();
+    assert_eq!(program.data, expected.data);
+
+    let expected = Program::from("3500,9,10,70,2,3,11,0,99,30,40,50".to_string());
+    program.step().unwrap();
+    assert_eq!(program.data, expected.data);
+
     Ok(())
 }
