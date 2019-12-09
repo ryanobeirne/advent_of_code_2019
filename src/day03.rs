a3462606624860dbfaca8ae9b9ec20efd574ce99
Ryan O'Beirne <ryanobeirne@ryanobeirne.com>
2019-12-09 17:43:36 -0500

    WIP

--- src/day03.rs
+++ src/day03.rs
@@ -6,9 +6,12 @@ use std::fmt;
 
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("input/day03.txt")?;
-    let wires: Vec<WireTrack> = input.lines()
-        .filter_map(|s| WireTrack::try_from(s).ok())
-        .collect();
+
+    for line in input.lines() {
+        let wire_track = WireTrack::try_from(line)?;
+        println!("{}", wire_track);
+    }
+
 
     answer!(03, 1, 42);
     Ok(())
@@ -67,14 +70,14 @@ impl WireTrack {
         self.map.iter()
             .max_by(|(point_a, _plot_a), (point_b, _plot_b)| point_a.y.cmp(&point_b.y))
             .expect("Empty WireTrack!")
-            .0.x
+            .0.y
     }
 
     fn right(&self) -> i16 {
         self.map.iter()
             .max_by(|(point_a, _plot_a), (point_b, _plot_b)| point_a.x.cmp(&point_b.x))
             .expect("Empty WireTrack!")
-            .0.y
+            .0.x
     }
 
     fn bot_right(&self) -> Point {
@@ -89,8 +92,8 @@ impl fmt::Display for WireTrack {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let right = self.right();
 
-        for x in self.left()..=right {
-            for y in self.top()..=self.bot() {
+        for y in self.top()..=self.bot() {
+            for x in self.left()..=right {
                 let point = Point { x, y, };
 
                 if let Some(plot) = self.map.get(&point) {
@@ -98,10 +101,11 @@ impl fmt::Display for WireTrack {
                 } else {
                     write!(f, ".")?;
                 }
-            }
 
-            if x == right {
-                writeln!(f)?;
+                if x >= right {
+                    write!(f, "\n")?;
+                }
+
             }
         }
 
@@ -112,7 +116,7 @@ impl fmt::Display for WireTrack {
 #[test]
 fn wire_track() -> Result<()> {
     let track = WireTrack::try_from("R8,U5,L5,D3")?;
-    println!("{}", track);
+    println!("WIRETRACK:\n{}", track);
     Ok(())
 }
 
@@ -138,7 +142,14 @@ impl From<Vector> for WireTrack {
     fn from(vector: Vector) -> WireTrack {
         let mut wire_track = WireTrack::new();
 
-       // Do stuff! 
+        let plot = match vector.turn.dir {
+            Direction::Left | Direction::Right => Plot::Horiz,
+            Direction::Down | Direction::Up => Plot::Vert,
+        };
+
+        for point in vector.points() {
+            wire_track.map.insert(point, plot);
+        }
 
         wire_track
     }
@@ -222,6 +233,53 @@ impl Vector {
 
     fn end(&self) -> Point {
         self.start.turn(self.turn)
+    }
+
+    fn points(&self) -> VectorPoints {
+        self.into_iter()
+    }
+}
+
+struct VectorPoints {
+    points: Vec<Point>,
+    index: usize,
+    len: usize,
+}
+
+impl Iterator for VectorPoints {
+    type Item = Point;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.len {
+            self.index += 1;
+            Some(self.points[self.index - 1])
+        } else {
+            None
+        }
+    }
+}
+
+impl IntoIterator for Vector {
+    type Item = Point;
+    type IntoIter = VectorPoints;
+    fn into_iter(self) -> Self::IntoIter {
+        let mut points = Vec::new();
+        let mut len = 0;
+
+        let mut start = self.start;
+        let end = self.end();
+        let dir = self.turn.dir;
+
+        while start != end {
+            start = start.turn(Turn::new(dir, 1));
+            points.push(start);
+            len += 1;
+        }
+
+        VectorPoints {
+            points,
+            len,
+            index: 0,
+        }
     }
 }
 
@@ -328,9 +386,5 @@ impl Distance for Vector {
     fn distance(&self, other: &Self) -> i16 {
         self.start.distance(&other.start)
     }
-}
-
-trait NextPoint {
-    fn next_point(&self) -> Point;
 }
 
