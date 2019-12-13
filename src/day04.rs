22842996cf76ec8d22c3ec6afbe647ba2cbd11ab
Ryan O'Beirne <ryanobeirne@ryanobeirne.com>
2019-12-13 15:19:20 -0500

    day04: part2

--- src/day04.rs
+++ src/day04.rs
@@ -1,12 +1,20 @@
 use advent_of_code_2019::*;
+use std::collections::HashMap;
 
 fn main() -> Result<()> {
+    // Puzzle input
     let range = 234208..=765869_i32;
 
-    let count = range.filter(|i| Digits::from(*i).is_possible_pwd())
+    let count = range.clone()
+        .filter(|i| Digits::from(*i).is_possible_pwd_part1())
         .count();
-        
     answer!(04, 1, count);
+
+    let count2 = range
+        .filter(|i| Digits::from(*i).is_possible_pwd_part2())
+        .count();
+    answer!(04, 2, count2);
+
     Ok(())
 }
 
@@ -17,16 +25,24 @@ struct Digits {
 }
 
 impl Digits {
-    fn is_possible_pwd(&self) -> bool {
+    fn is_possible_pwd_part1(&self) -> bool {
         self.non_decreasing() && self.has_double()
+    }
+
+    fn is_possible_pwd_part2(&self) -> bool {
+        self.non_decreasing() && self.has_double_not_more()
+    }
+
+    fn len(&self) -> usize {
+        self.digits.len()
     }
 
     fn non_decreasing(&self) -> bool {
         let mut peekable = self.digits.iter().peekable();
         while let Some(digit) = peekable.next() {
             if let Some(peek) = peekable.peek() {
-                if digit < peek {
-                    return false
+                if peek < &digit {
+                    return false;
                 }
             }
         }
@@ -44,15 +60,55 @@ impl Digits {
             }
         }
 
-       false
+        false
+    }
+
+    fn has_double_not_more(&self) -> bool {
+        // Counter for consecutive digits
+        let mut counter = HashMap::new();
+
+        // Loop through start indexes
+        for start in 0..=self.len()-2 {
+            let digit = self.digits[start];
+            // Loop through end indexes
+            for end in start+1..=self.len()-1 {
+                let len = end - start + 1;
+                if self.digits.iter()
+                    .skip(start)
+                    .take(len)
+                    .all(|d| d == &digit)
+                {
+                    if len > *counter.entry(digit).or_insert(0) {
+                        counter.insert(digit, len);
+                    }
+                }
+            }
+        }
+
+        counter.iter()
+            .any(|(_digit, count)| count == &2)
     }
 }
 
 #[test]
-fn has_double() {
-    assert!(Digits::from(111111).is_possible_pwd());
-    assert!(!Digits::from(223450).is_possible_pwd());
-    assert!(!Digits::from(123789).is_possible_pwd());
+fn non_decreasing() {
+    assert!(Digits::from(112233).non_decreasing());
+    assert!(Digits::from(123444).non_decreasing());
+    assert!(Digits::from(111122).non_decreasing());
+}
+
+#[test]
+fn is_possible_part2() {
+    assert!(Digits::from(112233).is_possible_pwd_part2());
+    assert!(!Digits::from(123444).is_possible_pwd_part2());
+    assert!(Digits::from(111122).is_possible_pwd_part2());
+}
+
+#[test]
+fn is_possible_part1() {
+    assert!(Digits::from(111111).is_possible_pwd_part1());
+    assert!(!Digits::from(223450).is_possible_pwd_part1());
+    assert!(!Digits::from(123789).is_possible_pwd_part1());
 }
 
 impl Iterator for Digits {
@@ -80,7 +136,7 @@ impl From<i32> for Digits {
     }
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 enum Digit {
     Zero  = 0,
     One   = 1,
@@ -114,5 +170,5 @@ impl From<char> for Digit {
 
 #[test]
 fn from_i32() {
-    let i = Digits::from(123456);
+    let _i = Digits::from(0123456789);
 }
