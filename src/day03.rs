e148187ce324e38c4af86559e381662270a91dd3
Ryan O'Beirne <robeirne@spoonflower.com>
2019-12-13 10:45:57 -0500

    day03: part02

--- src/day03.rs
+++ src/day03.rs
@@ -14,9 +14,11 @@ fn main() -> Result<()> {
 
     assert_eq!(tracks.len(), 2, "Looking for 2 Wire Tracks!");
 
-    let min_dist = tracks[0].closest_intersection(&tracks[1]);
-
+    let min_dist = tracks[0].closest_intersection_distance(&tracks[1]);
     answer!(03, 1, min_dist);
+
+    let min_steps = tracks[0].minimum_steps_to_intersect(&tracks[1]).expect("No intersections!");
+    answer!(03, 2, min_steps);
     Ok(())
 }
 
@@ -39,40 +41,86 @@ impl WireTrack {
         WireTrack::default()
     }
 
-    fn closest_intersection(&self, other: &Self) -> i16 {
+    fn intersections(&self, other: &Self) -> HashMap<Point, i16> {
         let mut matches = HashMap::new();
         for point in self.map.keys() {
             if point == &Point::default() { continue }
             if let Some(_plot) = other.map.get(point) {
-                matches.insert(point, point.distance(&Point::default()));
+                matches.insert(*point, point.distance(&Point::default()));
             }
         }
+        matches
+    }
 
-        matches.into_iter()
+    fn closest_intersection_distance(&self, other: &Self) -> i16 {
+        self.intersections(other).into_iter()
             .min_by(|(_pa, da), (_pb, db)| da.cmp(db))
             .expect("NO MATCHES FOUND")
             .1
     }
 
-    fn concat(&mut self, other: Self) {
-        for (point, plot) in other.map.into_iter() {
-            let checkplot = if point == Point::default() {
-                Plot::Start
-            } else if let Some(_plot) = self.map.get(&point) {
-               Plot::Intersect
-            } else {
-                plot
-            };
-
-            self.map.insert(point, checkplot);
-            for vector in other.vec.iter() {
-                self.vec.push(*vector);
-            }
-        }
-
+    fn minimum_steps_to_intersect(&self, other: &Self) -> Option<usize> {
+        self.intersections(other)
+                .iter()
+                .map(|(intersection, _len)| {
+                    self.steps_to_point(intersection).expect("Point doesn't exist!") +
+                    other.steps_to_point(intersection).expect("Point doesn't exist!")
+                })
+                .min()
     }
 
-    fn points(self) -> VectorPoints {
+    fn steps_to_point(&self, point: &Point) -> Option<usize> {
+        Some(self.points()
+            .enumerate()
+            .find(|(_i, p)| p == point)?
+            .0 + 1
+        )
+    }
+
+    fn concat(&mut self, other: Self) -> Option<Point> {
+        for vector in other.vec {
+            self.attach_vector(vector)?;
+        }
+
+        Some(self.end())
+    }
+
+    /// The last point in a WireTrack
+    fn end(&self) -> Point {
+        if self.vec.is_empty() {
+            Point::default()
+        } else {
+            self.vec.get(self.vec.len() - 1)
+                .expect("Couldn't get Point from WireTrack")
+                .end()
+        }
+    }
+    
+    /// Attach a Vector to the end of a WireTrack.
+    /// Returns None if the end of the current WireTrack is not the same point as the start of the
+    /// new Vector.
+    fn attach_vector(&mut self, vector: Vector) -> Option<Point> {
+        if self.end() != vector.start {
+            return None;
+        }
+
+        let plot = Plot::from(vector.turn.dir);
+
+        let mut points = vector.points();
+        if let Some(point) = points.next() {
+            self.map.insert(point, Plot::Turn);
+        }
+
+        for point in points {
+            self.map.insert(point, plot);
+        }
+
+        self.vec.push(vector);
+
+        Some(vector.end())
+    }
+
+    fn points(&self) -> VectorPoints {
         self.into_iter()
     }
 
@@ -147,6 +195,86 @@ impl fmt::Display for WireTrack {
 }
 
 #[test]
+fn vector_points() {
+    use std::iter::once;
+    let vector1 = Vector::new(Point::new(0,0), Turn::new(Direction::Right, 5));
+    let vector2 = Vector::new(vector1.end(), Turn::new(Direction::Down, 5));
+
+    let wire_track = once(vector1).chain(once(vector2)).collect::<WireTrack>();
+    assert_eq!(wire_track.steps_to_point(&Point::new(5,5)).unwrap(), 10);
+}
+
+#[test]
+fn steps_to_point() -> Result<()> {
+    let track1 = WireTrack::try_from("R8,U5,L5,D3")?;
+    let track2 = WireTrack::try_from("U7,R6,D4,L4")?;
+
+    dbg!(track1.points());
+
+    let steps_to_point1 = track1.steps_to_point(&Point::new(6, -5)).unwrap();
+    let steps_to_point2 = track2.steps_to_point(&Point::new(6, -5)).unwrap();
+
+    assert_eq!(steps_to_point1, 15);
+    assert_eq!(steps_to_point2, 15);
+
+    Ok(())
+}
+
+#[test]
+fn min_steps() -> Result<()> {
+    let track1 = WireTrack::try_from("R75,D30,R83,U83,L12,D49,R71,U7,L72")?;
+    let track2 = WireTrack::try_from("U62,R66,U55,R34,D71,R55,D58,R83")?;
+    let mut track1_clone = track1.clone();
+    track1_clone.concat(track2.clone());
+    println!("TRACK1:\n{}", &track1);
+    println!("TRACK1:\n{}", &track2);
+    println!("TRACK1+2:\n{}", track1_clone);
+
+    assert_eq!(track1.minimum_steps_to_intersect(&track2).unwrap(), 610);
+
+    let track3 = WireTrack::try_from("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")?;
+    let track4 = WireTrack::try_from("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")?;
+    println!("TRACK1:\n{}", &track3);
+    println!("TRACK1:\n{}", &track4);
+
+    assert_eq!(track3.minimum_steps_to_intersect(&track4).unwrap(), 410);
+
+    Ok(())
+}
+
+#[test]
+fn non_repeating_points() -> Result<()> {
+    let track1 = WireTrack::try_from("R75,D30,R83,U83,L12,D49,R71,U7,L72")?.points().peekable();
+    let track2 = WireTrack::try_from("U62,R66,U55,R34,D71,R55,D58,R83")?.points().peekable();
+    let track3 = WireTrack::try_from("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")?.points().peekable();
+    let track4 = WireTrack::try_from("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")?.points().peekable();
+
+    for track in [track1, track2, track3, track4].iter_mut() {
+        while let Some(next) = track.next() {
+            if let Some(peek) = track.peek() {
+                assert_ne!(&next, peek, "Consecutive points repeat!");
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[test]
+fn intersections() -> Result<()> {
+    let track1 = WireTrack::try_from("R8,U5,L5,D3")?;
+    let track2 = WireTrack::try_from("U7,R6,D4,L4")?;
+
+    let intersections = track1.intersections(&track2);
+    assert_eq!(intersections.len(), 2);
+
+    let min_steps = track1.minimum_steps_to_intersect(&track2).expect("No intersections");
+    assert_eq!(min_steps, 30);
+
+    Ok(())
+}
+
+#[test]
 fn wire_track() -> Result<()> {
     let mut track1 = WireTrack::try_from("R8,U5,L5,D3")?;
     let track2 = WireTrack::try_from("U7,R6,D4,L4")?;
@@ -213,7 +341,6 @@ struct Point {
 }
 
 impl Point {
-    #[cfg(test)]
     fn new(x: i16, y: i16) -> Self {
         Point { x, y }
     }
@@ -312,7 +439,7 @@ impl Iterator for VectorPoints {
     }
 }
 
-impl IntoIterator for Vector {
+impl IntoIterator for &Vector {
     type Item = Point;
     type IntoIter = VectorPoints;
     fn into_iter(self) -> Self::IntoIter {
@@ -337,7 +464,7 @@ impl IntoIterator for Vector {
     }
 }
 
-impl IntoIterator for WireTrack {
+impl IntoIterator for &WireTrack {
     type Item = Point;
     type IntoIter = VectorPoints;
     fn into_iter(self) -> Self::IntoIter {
@@ -359,9 +486,21 @@ impl FromIterator<Point> for VectorPoints {
     }
 }
 
+impl FromIterator<Vector> for WireTrack {
+    fn from_iter<I: IntoIterator<Item=Vector>>(iter: I) -> Self {
+        let mut wire_track = WireTrack::default();
+        for vector in iter.into_iter() {
+            wire_track.concat(WireTrack::from(vector));
+        }
+
+        wire_track
+    }
+}
+
 #[test]
 fn points_iter() {
     let vector = Vector::new(Point::new(0,0), Turn::new(Direction::Right, 5));
+    assert_eq!(vector.points().count(), 5);
     for point in vector.points() {
         dbg!(point);
     }
@@ -402,6 +541,15 @@ enum Plot {
     Vert,
     Turn,
     Intersect,
+}
+
+impl From<Direction> for Plot {
+    fn from(dir: Direction) -> Plot {
+        match dir {
+            Direction::Left | Direction::Right => Plot::Horiz,
+            Direction::Up   | Direction::Down  => Plot::Vert,
+        }
+    }
 }
 
 impl fmt::Display for Plot {
